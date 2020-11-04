a0bf59454e65a30282fdbe3ed361d7b35a4f5cd8
kak79 <kohelka@gmail.com>
2020-11-04 11:28:26 -0600

    login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def login
+    @username = User.find_by(username: params[:username])
+    if @username && @username.authenticate(params[:password])
+      session[:username_id] = @username.id
+      redirect_to user_path(@user)
+    end
+  end
+
+
 end
