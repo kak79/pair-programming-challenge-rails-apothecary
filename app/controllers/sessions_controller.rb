787b851217e5eba3d929f2ce94563c40d56c8d75
miloscomplex <hicallmesutton@gmail.com>
2020-11-04 13:05:03 -0600

    modified login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,13 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
     byebug
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:username_id] = @user.id
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:username_id] = user.id
       redirect_to user_path(@user)
     else
       render :new
