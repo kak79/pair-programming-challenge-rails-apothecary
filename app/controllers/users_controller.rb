cb70701f12764636f877d769c201a83a7093c985
kak79 <kohelka@gmail.com>
2020-11-04 11:29:08 -0600

    too much for one message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,61 +1,27 @@
 class UsersController < ApplicationController
 
-  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit]
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
+    if @user = User.find_by(username: params[:username]).nil? && User.find_by(username: params[:username]).nil?
+      @username = User.new(user_params)
+      if @username.save
+        session[:username_id] = @username.id
+        redirect_to user_path(@username)
+      end
     end
+    render :new
   end
 
   def index
     @user = User.all
   end
 
-  post "/registration/signup" do
-  #check if the email name combo exists -- done
-  #should the email be made lowercase --- stretch feature
-  #name cannot contain numbers, periods, etc -- done
-   #should it be forced to initial cap? --- some people have odd names
-  # need to fully railize this !!!
-  if @user = User.find_by(username: params[:username]).nil? && User.find_by(username: params[:username]).nil?
-    @username = User.new(name: params[:name], email: params[:email], password: params[:password])
-    # binding.pry
-    if @username.save
-      session[:username_id] = @username.id
-      redirect user_path(@username)
-    end
-  end
-  render :"/users/new"
-end
-
-post login do
-  @username = User.find_by(username: params[:username])
-  if @username && @username.authenticate(params[:password])
-    session[:username_id] = @username.id
-    redirect # users/:id
-  end
-end
-
-
-
-
-  # def update
-  #   @user.update(user_params)
-  #   redirect_to user_path(@user)
-  # end
-
-  # def destroy
-  #   @user.destroy
-  #   redirect_to users_path
-  # end
+ 
 
   private
 
