037deec6665bc65bf5da961b96c1bcf9ab912b38
miloscomplex <hicallmesutton@gmail.com>
2020-11-04 12:33:59 -0600

    created sessions_controller aand session views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:username_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to login_path
+  end
+
+  private
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @user ||= User.find_by_id(session[:user_id])
+  end
+
+  def verify
+    if !logged_in?
+      redirect_to users_path
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-resources :treatments
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+  resources :treatments
 
   resources :users do
     resources :prescriptions
   end
-
-  
-    get "/login" => "sessions#new"
-    post "/login" => "sessions#create"
-    delete "/logout" => "sessions#destroy"
-  
-
 
 end
