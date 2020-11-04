9d7f0d66775a4df192c9ce5d9595d8cc54478d36
brewchetta <chett.tiller@gmail.com>
2020-11-04 15:11:06 -0500

    foreign keysgit add .git add .

--- db/migrate/20201029163327_create_prescriptions.rb
+++ db/migrate/20201029163327_create_prescriptions.rb
@@ -1,7 +1,8 @@
 class CreatePrescriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :prescriptions do |t|
-
+      t.integer :treatment_id
+      t.integer :user_id
       t.timestamps
     end
   end
