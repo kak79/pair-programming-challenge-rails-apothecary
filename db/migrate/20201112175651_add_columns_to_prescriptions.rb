85ca901b8cea7da15d03334a2f48b50795881bfe
kak79 <kohelka@gmail.com>
2020-11-12 16:35:27 -0600

    extra ids, not needed

--- db/migrate/20201112175651_add_columns_to_prescriptions.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToPrescriptions < ActiveRecord::Migration[6.0]
-  def change
-    add_column :prescriptions, :user_id, :integer
-    add_column :prescriptions, :treatment_id, :integer
-  end
-end
